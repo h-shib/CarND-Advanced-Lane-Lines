1d13467bb1dc0c664b2dc2e31ca3345de416cfe1
h-shib <hiraku.shibuya888@gmail.com>
2017-02-19 16:55:45 +0900

    add draw lane line function

--- lane_finding.py
+++ lane_finding.py
@@ -51,8 +51,9 @@ def perspective_transform(undist_img):
 						[img_size[0]-offset, img_size[1]],
 						[offset, img_size[1]]])
 	M = cv2.getPerspectiveTransform(src, dst)
+	Minv = cv2.getPerspectiveTransform(dst, src)
 	warped = cv2.warpPerspective(undist_img, M, img_size)
-	return warped
+	return warped, Minv
 
 def mag_thresh(img, sobel_kernel=3, thresh=(0, 255)):
 	# Calculate gradient magnitude
@@ -92,7 +93,27 @@ def apply_threshold(img, sobel_kernel=3):
 	combined_binary[((mag_binary == 1) & (dir_binary == 1)) | (s_binary == 1)] = 1
 	return combined_binary
 
-def find_lines(binary_warped):
+
+def draw_lane_line(warped, left_fitx, right_fitx, ploty, Minv, image, undist):
+	warp_zero = np.zeros_like(warped).astype(np.uint8)
+	color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+
+	# Recast the x and y points into usable format for cv2.fillPoly()
+	pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+	pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+	pts = np.hstack((pts_left, pts_right))
+
+	# Draw the lane onto the warped blank image
+	cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
+
+	# Warp the blank back to original image space using inverse perspective matrix (Minv)
+	newwarp = cv2.warpPerspective(color_warp, Minv, (image.shape[1], image.shape[0])) 
+	# Combine the result with the original image
+	result = cv2.addWeighted(undist, 1, newwarp, 0.3, 0)
+	plt.imshow(result)
+	plt.show()
+
+def find_lines(binary_warped, Minv, image, undist):
 	# Assuming you have created a warped binary image called "binary_warped"
 	# Take a histogram of the bottom half of the image
 	histogram = np.sum(binary_warped[binary_warped.shape[0]/2:,:], axis=0)
@@ -174,6 +195,8 @@ def find_lines(binary_warped):
 	plt.xlim(0, 1280)
 	plt.ylim(720, 0)
 
+	draw_lane_line(binary_warped, left_fitx, right_fitx, ploty, Minv, image, undist)
+
 
 def main():
 	# run pipeline
@@ -184,13 +207,13 @@ def main():
 	imgpoints = dist_data['imgpoints']
 
 	#image = plt.imread('test_images/straight_lines2.jpg')
-	image = plt.imread('test_images/test2.jpg')
+	image = plt.imread('test_images/straight_lines2.jpg')
 	plt.imshow(image)
 	plt.show()
 	dst = undistort(image, objpoints, imgpoints)
 	thresh_binary = apply_threshold(dst)
-	warped_binary = perspective_transform(thresh_binary)
-	find_lines(warped_binary)
+	warped_binary, Minv = perspective_transform(thresh_binary)
+	find_lines(warped_binary, Minv, image, dst)
 	plt.imshow(warped_binary, cmap='gray')
 	plt.show()
 
